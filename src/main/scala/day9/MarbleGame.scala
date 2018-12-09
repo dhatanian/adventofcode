067c0515ede173929f233916812ebc28b6d7c0bb
David Hatanian <david@hatanian.com>
2018-12-09 23:14:05 +0000

    Day 9 - first solution

--- src/main/scala/day9/MarbleGame.scala
+++ src/main/scala/day9/MarbleGame.scala
@@ -2,22 +2,28 @@ package day9
 
 class MarbleGame(players: Int, lastMarble: Int) {
   private var marbles = List[Int](0)
-  private var currentMarbleIndex = 0
+  private var currentMarbleIndex: Int = 0
   private var playersScores = Array.fill(players)(0)
 
   def findHighScore(): Int = {
     for (marble <- 1 to lastMarble) {
+      println(marble)
       val player = ((marble - 1) % players) + 1
       if (marble % 23 == 0) {
-        playersScores(player -1) = marble
+        currentMarbleIndex = (currentMarbleIndex - 7) % marbles.length
+        while (currentMarbleIndex < 0) {
+          currentMarbleIndex += marbles.length
+        }
+        playersScores(player - 1) = playersScores(player - 1) + marble + marbles(currentMarbleIndex)
+        marbles = marbles.slice(0, currentMarbleIndex) ++ marbles.slice(currentMarbleIndex + 1, marbles.length)
       } else {
         currentMarbleIndex = (currentMarbleIndex + 2) % marbles.length
         marbles = (marbles.slice(0, currentMarbleIndex) :+ marble) ++ marbles.slice(currentMarbleIndex, marbles.length)
-        println(marbles)
-        println(playersScores.toList)
+//        println(marbles)
+//        println(playersScores.toList)
       }
     }
 
-    0
+    playersScores.max
   }
 }
--- src/main/scala/day9/Solution.scala
+++ src/main/scala/day9/Solution.scala
@@ -3,7 +3,7 @@ package day9
 object Solution {
 
   def main(args: Array[String]): Unit = {
-    println(new MarbleGame(9, 25).findHighScore())
-//    println(new MarbleGame(476, 71657).findHighScore())
+//    println(new MarbleGame(9, 25).findHighScore())
+    println(new MarbleGame(476, 71657).findHighScore())
   }
 }
