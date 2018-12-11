abd89552ae61fd1021b754b07a4fc117d64c499e
David Hatanian <david@hatanian.com>
2018-12-11 13:05:08 +0000

    Day 2 very slow solution

--- src/main/scala/day11/Solution.scala
+++ src/main/scala/day11/Solution.scala
@@ -4,6 +4,8 @@ object Solution {
 
   val serialNumber = 9005
 
+  val currentPowers = Array.fill(300, 300)(0L)
+
   def power(x: Int, y: Int): Long = {
     val rackId = x + 10
     val startPowerLevel = rackId * y
@@ -13,29 +15,31 @@ object Solution {
     hundredsDigits - 5
   }
 
-  def main(args: Array[String]): Unit = {
-    val currentPowers = Array.fill(300, 300)(0L)
-
-    for (x <- 1 to 300) {
-      for (y <- 1 to 300) {
-        currentPowers(x - 1)(y - 1) = power(x, y)
-      }
-    }
-
+  def highestPowerLevelForGridSize(gridSize: Int) = {
+    println(gridSize)
     var highestPowerLevel = Long.MinValue
     var highestPowerLevelCoordinates = (0, 0)
-    for (x <- 0 to 297) {
-      for (y <- 0 to 297) {
-        val totalPower = currentPowers(x)(y) + currentPowers(x + 1)(y) + currentPowers(x + 2)(y) + currentPowers(x)(y + 1) + currentPowers(x + 1)(y + 1) + currentPowers(x + 2)(y + 1) + currentPowers(x)(y + 2) + currentPowers(x + 1)(y + 2) + currentPowers(x + 2)(y + 2)
+    for (x <- 0 to 300 - gridSize) {
+      for (y <- 0 to 300 - gridSize) {
+        val totalPower = (0 until gridSize).flatMap(dx => (0 until gridSize).map(dy => currentPowers(x + dx)(y + dy))).sum
         if (totalPower > highestPowerLevel) {
           highestPowerLevel = totalPower
           highestPowerLevelCoordinates = (x + 1, y + 1)
         }
       }
     }
+    (highestPowerLevel, highestPowerLevelCoordinates)
+  }
 
+  def main(args: Array[String]): Unit = {
+    for (x <- 1 to 300) {
+      for (y <- 1 to 300) {
+        currentPowers(x - 1)(y - 1) = power(x, y)
+      }
+    }
 
-    println(highestPowerLevel)
-    println(highestPowerLevelCoordinates)
+    val result = (1 to 300).map(highestPowerLevelForGridSize).maxBy(_._1)
+
+    println(result)
   }
 }
